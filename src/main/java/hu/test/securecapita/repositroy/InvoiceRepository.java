66ce9e9a8ce07394fb239d54009ff81486830613
Kristof-Toth <tothkristof98@gmail.com>
2023-07-27 14:29:50 +0200

    customer & invoice

---
+++ src/main/java/hu/test/securecapita/repositroy/InvoiceRepository.java
@@ -0,0 +1,8 @@
+package hu.test.securecapita.repositroy;
+
+import hu.test.securecapita.domain.Invoice;
+import org.springframework.data.repository.ListCrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+public interface InvoiceRepository extends PagingAndSortingRepository<Invoice, Long>, ListCrudRepository<Invoice, Long> {
+}
